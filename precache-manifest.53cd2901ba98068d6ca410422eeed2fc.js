dc14ff0622049418facaf5f442251c5521f16489
Francisco Javier Rojas M <javier32rojas040506@gmail.com>
2022-08-05 00:05:28 -0500

    Updates

---
+++ precache-manifest.53cd2901ba98068d6ca410422eeed2fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9686b65459437b1a2818",
+    "url": "/javier32rojas040506.github.io/TodoList/static/css/main.da105962.chunk.css"
+  },
+  {
+    "revision": "9686b65459437b1a2818",
+    "url": "/javier32rojas040506.github.io/TodoList/static/js/main.9686b654.chunk.js"
+  },
+  {
+    "revision": "481429cb0ee277edd7fd",
+    "url": "/javier32rojas040506.github.io/TodoList/static/js/1.481429cb.chunk.js"
+  },
+  {
+    "revision": "53aeaf80a30438f789bf",
+    "url": "/javier32rojas040506.github.io/TodoList/static/js/runtime~main.53aeaf80.js"
+  },
+  {
+    "revision": "31d4958bef0f6b2ae7ae9e8eda93a333",
+    "url": "/javier32rojas040506.github.io/TodoList/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/javier32rojas040506.github.io/TodoList/precache-manifest.53cd2901ba98068d6ca410422eeed2fc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/javier32rojas040506.github.io/TodoList/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
